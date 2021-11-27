2339180f33aa9ccb54b353de7ed4cf6e34cf512c
yawar2019 <yawarali17@gmail.com>
2021-11-27 21:59:47 +0530

    comment

--- MVCMaster2/Controllers/DefaultController.cs
+++ MVCMaster2/Controllers/DefaultController.cs
@@ -112,5 +112,72 @@ namespace MVCMaster2.Controllers
             return View(listemp);
         }
 
+
+
+        public ActionResult SendMultipleDataMethod3()
+        {
+            List<EmployeeModel> listemp = new List<EmployeeModel>();
+
+
+            EmployeeModel emp = new EmployeeModel();
+            emp.EmpId = 1;
+            emp.EmpName = "Sai";
+            emp.EmpSalary = 34331;
+
+
+            EmployeeModel emp1 = new EmployeeModel();
+            emp1.EmpId = 2;
+            emp1.EmpName = "Jagdish";
+            emp1.EmpSalary = 44331;
+
+
+            EmployeeModel emp2 = new EmployeeModel();
+            emp2.EmpId = 3;
+            emp2.EmpName = "Gaurav";
+            emp2.EmpSalary = 54331;
+
+
+            listemp.Add(emp);
+            listemp.Add(emp1);
+            listemp.Add(emp2);
+
+            // ------------------------
+
+            DepartmentModel dept = new DepartmentModel();
+            dept.DeptId = 1;
+            dept.DeptName = "IT";
+
+            DepartmentModel dept2 = new DepartmentModel();
+            dept2.DeptId = 2;
+            dept2.DeptName = "Marketing";
+
+            List<DepartmentModel> listdept = new List<DepartmentModel>();
+            listdept.Add(dept);
+            listdept.Add(dept2);
+
+            EmpDept ed = new EmpDept();
+            ed.emp = listemp;
+            ed.dept = listdept;
+
+            return View(ed);
+        }
+
+
+        public ActionResult MulipleOutput()
+        {
+            return Redirect("http://www.google.com");
+        }
+
+
+        public ActionResult MulipleOutput1()
+        {
+            return Redirect("~/Default/MulipleOutput2");
+        }
+
+
+        public ActionResult MulipleOutput2()
+        {
+            return Content("hello..");
+        }
     }
 }
---
+++ MasterClass9pmBatch/Models/EmpDept.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MasterClass9pmBatch.Models
+{
+    public class EmpDept
+    {
+        public List<EmployeeModel> emp { get; set; }
+        public List<DepartmentModel> dept { get; set; }
+    }
+}
