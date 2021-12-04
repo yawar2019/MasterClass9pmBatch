f3bb9c897de8f14d560fbc03481ce62728f0d19d
yawar2019 <yawarali17@gmail.com>
2021-12-04 22:15:28 +0530

    Committrd

---
+++ AdoDotNet/Controllers/DefaultController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.SqlClient;
+using AdoDotNet.Models;
+
+namespace AdoDotNet.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default 1;Delete from Employee
+
+        EmployeeContext db = new EmployeeContext();
+        public ActionResult Index()
+        { 
+
+            return View(db.GetEmployees());
+        }
+    }
+}
+
+//Connection Oriented           Disconnected Oriented Architecture 
+
+//    1)sqlcommand                         1)sqldataAdapter
+
+//     2)Datareader                             Dataset
+
+    
---
+++ AdoDotNet/Models/EmployeeContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace AdoDotNet.Models
+{
+    public class EmployeeContext
+    {
+        //ConnectionString
+        SqlConnection con = new SqlConnection(@"Data Source=AZAM-PC\SQLEXPRESS;Initial Catalog=YashDB;Integrated Security=true");
+
+        public List<EmployeeModel> GetEmployees()
+        {
+            List<EmployeeModel> listemp = new List<EmployeeModel>();
+            SqlCommand cmd = new SqlCommand("usp_getEmployeeDetails",con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            foreach (DataRow dr in dt.Rows)//3
+            {
+                EmployeeModel emp = new EmployeeModel();
+                emp.EmpId = Convert.ToInt32(dr[0]);
+                emp.EmpName = Convert.ToString(dr[1]);
+                emp.EmpSalary = Convert.ToInt32(dr[2]);
+                listemp.Add(emp);
+            }
+
+            return listemp;
+        }
+    }
+}
