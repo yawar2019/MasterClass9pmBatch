a258239eaa06a3a2f1cb29c893d861090c1d1cc8
yawar2019 <yawarali17@gmail.com>
2021-12-10 21:55:02 +0530

    Committed

---
+++ DapperExample/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using DapperExample.Models;
+
+namespace DapperExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+
+        EmployeeContext db = new EmployeeContext();
+        public ActionResult Index()
+        {
+            return View(db.GetEmployees());
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(EmployeeModel emp)
+        {
+            int result = db.SaveEmployee(emp);
+            if (result > 0)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
---
+++ DapperExample/Models/EmployeeContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using Dapper;
+namespace DapperExample.Models
+{
+
+    public class EmployeeContext
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=AZAM-PC\SQLEXPRESS;Initial Catalog=Employee;Integrated Security=true");
+
+        public List<EmployeeModel> GetEmployees()
+        {
+           return  con.Query<EmployeeModel>("sp_getEmployee_Rock", commandType:System.Data.CommandType.StoredProcedure).ToList();
+        }
+
+       public int SaveEmployee(EmployeeModel emp)
+        {
+            var parameter = new DynamicParameters();
+            parameter.Add("@EmpName",emp.EmpName);
+            parameter.Add("@EmpSalary",emp.EmpSalary);
+            int result = con.Execute("sp_InsertEmployee",param:parameter, commandType: System.Data.CommandType.StoredProcedure);
+            return result;
+        }
+    }
+}
