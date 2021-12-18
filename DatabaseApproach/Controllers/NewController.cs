93fbe47a2aea7955c22ff49acdf8b087c74fe5ef
yawar2019 <yawarali17@gmail.com>
2021-12-18 22:01:42 +0530

    committed

---
+++ DatabaseApproach/Controllers/NewController.cs
@@ -0,0 +1,24 @@
+﻿using DatabaseApproach.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DatabaseApproach.Controllers
+{
+    public class NewController : Controller
+    {
+        // GET: New
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(RegisterModel reg)
+        {
+            return View();
+        }
+    }
+}
---
+++ DatabaseApproach/Models/RegisterModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DatabaseApproach.Models
+{
+    public class RegisterModel
+    {
+        public int UserId { get; set; }
+        [Required(ErrorMessage ="UserName Cannot Be Empty")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Password Cannot Be Empty")]
+        public string Password { get; set; }
+        [Compare("Password",ErrorMessage = "Mismatch of Password and Confirm Password")]
+        public string ConfirmPassword { get; set; }
+        [Range(18,45,ErrorMessage ="18-45 can Apply for Job")]
+        public int Age { get; set; }
+        [DataType(DataType.EmailAddress,ErrorMessage ="Email ID Format is not Correct")]
+        public string EmailId { get; set; }
+        [StringLength(10,ErrorMessage ="Not Allowed more then 10 character")]
+        public string Address { get; set; }
+
+    }
+}
