76a6a1ce6483e8ed39b2b78f36132c23b9335abf
yawar2019 <yawarali17@gmail.com>
2021-12-20 21:48:38 +0530

    committed

---
+++ CodeFirstApproach/Controllers/EmployeesController.cs
@@ -0,0 +1,33 @@
+﻿using CodeFirstApproach.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CodeFirstApproach.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        EmployeeContext db = new EmployeeContext();
+        // GET: Employees
+        public ActionResult Index()
+        {
+            return View(db.EmployeeModels.ToList());
+        }
+
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(EmployeeModel emp)
+        {
+            db.EmployeeModels.Add(emp);//insert query
+            db.SaveChanges();//Execute Query
+            return RedirectToAction("Index");
+        }
+
+    }
+}
