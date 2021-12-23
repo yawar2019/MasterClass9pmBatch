67720b0657605c3d7c1ad33f188020e0aae0648b
yawar2019 <yawarali17@gmail.com>
2021-12-23 22:02:06 +0530

    committed

---
+++ TestLinqApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLinqApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 1, 2, 3, 4, 5 };
+            string[] b = { "james", "radha","virat", "Dilip", "Keshav" };
+
+
+            //var result = a.Where(s => s > 2);
+            //var result = b.Contains("veirat");
+            var result = b.OrderByDescending(x=>x);
+
+
+
+
+            // var result = from n in a
+            //              //where n>2 && n<4
+            //               orderby n descending select n;
+            // //  var result2 = result.Sum();
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+
+            }
+            //Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
