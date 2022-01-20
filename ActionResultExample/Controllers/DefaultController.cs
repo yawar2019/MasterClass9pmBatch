5d23d0e29e8910ff4bd71f756d55e63648db1564
yawar2019 <yawarali17@gmail.com>
2022-01-20 22:04:00 +0530

    committed

---
+++ ActionResultExample/Controllers/DefaultController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using Dapper;
+using System.Data.SqlClient;
+using ActionResultExample.Models;
+using ActionResultExample.CalculatorServiceReference;
+using ActionResultExample.WCFServiceReference;
+
+namespace ActionResultExample.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ContentResult GetDifferentContent(int? id)
+        {
+            if (id == 1)
+            {
+
+                return Content("Hello World");
+            }
+
+            else if (id == 2)
+            {
+                return Content("<p style=color:red>Hello World</p>");
+            }
+
+            else
+            {
+                return Content("<script>alert('Hi')</script>");
+            }
+        }
+
+
+
+
+
+        public RedirectToRouteResult RedirectMetoRoute()
+
+        {
+
+            return RedirectToRoute("Default2");
+
+        }
+
+
+
+
+        public RedirectToRouteResult RedirectMetoRoute2()
+
+        {
+
+            return RedirectToAction("index", "Default");
+
+        }
+
+        //jumping from OneAction Method to Another we canuse RedirectToAction
+
+
+
+        public ActionResult GetDataFromDapper()
+        {
+            SqlConnection con = new SqlConnection("Data Source=AZAM-PC\\SQLEXPRESS;Initial Catalog=Employee;Integrated Security=true ");
+            var result = con.Query<EmployeeModel>("Select EmpId,EmpName,EmpSalary from [Employee].[dbo].[employeeDetails]");
+
+
+            return View();
+
+
+        }
+
+
+        public ActionResult GetCalculatorService()
+        {
+            CalculatorServiceSoapClient obj = new CalculatorServiceSoapClient();
+            string result = obj.AddCalculator(12, 20, "+");
+            return Content(result);
+        }
+        public ActionResult GetWCFService()
+        {
+            WCFServiceReference.Service1Client obj = new Service1Client();
+            int result = obj.Add(12, 20);
+            return Content(result.ToString());
+        }
+
+
+    }
+}
---
+++ WebServicesExample/CalculatorService.asmx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace WebServicesExample
+{
+    /// <summary>
+    /// Summary description for CalculatorService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class CalculatorService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+        [WebMethod]
+        public   string AddCalculator(int a,int b,string oper)
+        {
+            int result=0;
+            switch (oper) {
+
+                case "+":
+                    result = a + b;
+                    break;
+                case "-":
+                    result = a - b;
+                    break;
+                case "*":
+                    result = a * b;
+                    break;
+                case "/":
+                    result = a / b;
+                    break;
+                default:
+                    result = 0;
+                    break;
+            }   
+            return "result:"+result;
+        }
+    }
+}
